b0d58dcb4f5b09f283a144a4553202447f7204ac
saimadhava-cyber <saimadhava287@gmail.com>
2024-08-06 09:29:15 +0530

    intersection of two list.py

---
+++ intersection of two list.py
@@ -0,0 +1,6 @@
+Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======== RESTART: C:/Users/Windows/Downloads/python/union of two list.py =======
+[3, 4, 5]
+>>> 
